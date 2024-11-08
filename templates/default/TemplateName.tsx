c3a2e8c0a7d86e22d11fdabb5f1b0ecae31eb8cf
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-08 23:04:36 +0100

    style: format code and update README for consistency

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import styles from "./TemplateName.module.scss";
 
 const TemplateName: React.FC = () => {
-  return (
-    <div
-      className={styles.TemplateName}
-    >
-      TemplateName Component
-    </div>
-  );
+  return <div className={styles.TemplateName}>TemplateName Component</div>;
 };
 
 export default TemplateName;
