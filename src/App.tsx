1426141141da4dca94a9baa1d7da278ca3cbe45f
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-09 22:24:42 +0100

    feat: implement routing and layout structure in App component with SCSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import React from "react";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import LoginLayout from "./components/LoginLayout/LoginLayout";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import styles from "./App.module.scss";
 
-function App() {
-  return <div>Hello, World!</div>;
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    element: <LoginLayout/>,
+    children: [
+      {
+        id: "login",
+        path: "login",
+        element: <SignIn />
+      },
+      {
+        id: "register",
+        path: "register",
+        element: <SignUp />
+      }
+    ]
+  }
+]);
+function App(): React.JSX.Element {
+  return (
+    <section className={styles.App}>
+      <Header />
+      <RouterProvider
+        router={router}
+        fallbackElement={<p>Initial Load...</p>}
+      />
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
