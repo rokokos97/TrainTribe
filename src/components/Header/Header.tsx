1d155262713ae58ae41f2ccf8ce944eb7a3c187e
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-10 13:03:57 +0100

    style: remove border from Header component and adjust title styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,6 @@ const Header: React.FC = () => {
   return (
     <div className={styles.Header}>
       <p className={styles.title}>train tribe</p>
-      <p className={styles.subtitle}>join the tribe, share the stride</p>
     </div>
   );
 };
