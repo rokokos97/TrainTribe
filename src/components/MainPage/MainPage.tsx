30329ac846aee0677f91282f338b3820a9bfe872
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-11 23:56:59 +0100

    feat: add MainPage component with styles and tests

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./MainPage.module.scss";
+
+const MainPage: React.FC = () => {
+  return <div className={styles.MainPage}>MainPage Component</div>;
+};
+
+export default MainPage;
