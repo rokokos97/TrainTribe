3b089c47baeed037d405b483d85245a5697083cc
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-11 23:54:39 +0100

    refactor: replace RouterProvider with Routes and Route for routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import Layout from "./components/Layout/Layout";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import styles from "./App.module.scss";
+import { Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    id: "root",
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        id: "login",
-        path: "login",
-        element: <SignIn />
-      },
-      {
-        id: "register",
-        path: "register",
-        element: <SignUp />
-      }
-    ]
-  }
-]);
+import Layout from "./components/Layout/Layout";
 function App(): React.JSX.Element {
   return (
     <section className={styles.App}>
-      <Header />
-      <RouterProvider
-        router={router}
-        fallbackElement={<p>Initial Load...</p>}
-      />
-      <Footer />
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout />}
+        >
+          {/* <Route
+            index
+            element={<Layout />}
+          /> */}
+        </Route>
+      </Routes>
     </section>
   );
 }
