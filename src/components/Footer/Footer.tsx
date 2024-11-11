c6281e50fb92adf1253e8fe7e7d0e46e4c394701
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-12 00:21:55 +0100

    feat: replace buttons with Link components in Footer for improved navigation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
     <div className={styles.Footer}>
       <section className={styles.section}>
-        <button className={styles.buttonSmall}>about</button>
-        <button className={styles.button}>tarm & conditions</button>
-        <button className={styles.buttonSmall}>help</button>
+        <Link
+          to="/about"
+          className={styles.buttonSmall}
+        >
+            about
+        </Link>
+        <Link
+          to="/term&conditions"
+          className={styles.button}
+        >
+          tarm & conditions
+        </Link>
+        <Link
+          to="/help"
+          className={styles.buttonSmall}
+        >
+          help
+        </Link>
       </section>
       <p>© 2024 TrainTribe</p>
     </div>
