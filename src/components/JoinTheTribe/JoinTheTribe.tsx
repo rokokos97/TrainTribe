d83145704b19adac950e00448c40865a461f52b6
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-12 16:25:07 +0100

    feat: add JoinTheTribe component with styling and tests

---
+++ src/components/JoinTheTribe/JoinTheTribe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./JoinTheTribe.module.scss";
+
+const JoinTheTribe: React.FC = () => {
+  return (
+    <section className={styles.JoinTheTribe}>
+      Here will be form to propose a new training session.
+    </section>
+  );
+};
+
+export default JoinTheTribe;
