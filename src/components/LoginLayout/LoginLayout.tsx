889d2d75477ce97995c89ba3fde0338111c8432a
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-12 16:27:07 +0100

    feat: enhance LoginLayout with routing and styling adjustments

--- src/components/LoginLayout/LoginLayout.tsx
+++ src/components/LoginLayout/LoginLayout.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import styles from "./LoginLayout.module.scss";
 
+import { Routes, Route } from "react-router-dom";
+
+import SignUp from "../SignUp/SignUp";
+import SignIn from "../SignIn/SignIn";
+import Page404 from "../Page404/Page404";
+
 const LoginLayout: React.FC = () => {
-  return <div className={styles.LoginLayout}></div>;
+  return <div className={styles.LoginLayout}>
+    <h2 className={styles.subtitle}>Hello and Welcome</h2>
+    <Routes>
+      <Route
+        index
+        element={<SignIn />}
+      />
+      <Route
+        path="/signUp"
+        element={<SignUp />}
+      />
+      <Route
+        path="*"
+        element={<Page404 />}
+      />
+    </Routes>
+  </div>;
 };
 
 export default LoginLayout;
