a6889c0a77e331b86cabf1435f19ce2621a311a5
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-30 18:30:11 +0100

    refactor: remove unused components and their associated styles and tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,14 @@ import React from "react";
 import styles from "./App.module.scss";
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./components/Layout/Layout";
-import MainPage from "./components/MainPage/MainPage";
-import JoinTheTribe from "./components/JoinTheTribe/JoinTheTribe";
-import ShareTheStribe from "./components/ShareTheStribe/ShareTheStribe";
-import Page404 from "./components/Page404/Page404";
-import LoginLayout from "./components/LoginLayout/LoginLayout";
 function App(): React.JSX.Element {
   return (
     <section className={styles.App}>
       <Routes>
-        <Route
-          path="/"
-          element={<Layout />}
-        >
-          <Route
-            index
-            element={<MainPage />}
-          />
-          <Route
-            path="/joinTheTribe"
-            element={<JoinTheTribe />}
-          />
-          <Route
-            path="/shareTheStribe"
-            element={<ShareTheStribe />}
-          />
-          <Route
-            path="/signIn/*"
-            element={<LoginLayout />}
-          />
-          <Route
-            path="*"
-            element={<Page404 />}
-          />
+        <Route>
         </Route>
       </Routes>
+      Hello world
     </section>
   );
 }
