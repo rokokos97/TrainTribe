6899438961dabfa414944376fe6347483bb4e079
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 11:02:59 +0100

    style: format Vite config for improved readability

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@use "./src/styles/variables.scss" as * ;'
+        additionalData:
+          '@use "./src/styles/variables.scss" as * ;'
       }
     }
   }
